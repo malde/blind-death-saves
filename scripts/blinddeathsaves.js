c67c421d0a499630de9f8aa97c2d8e7843271f52
Aleksandr Vasilenko <phenomen@proton.me>
2022-10-11 09:45:31 -0500

    Update blinddeathsaves.js

--- scripts/blinddeathsaves.js
+++ scripts/blinddeathsaves.js
@@ -10,3 +10,29 @@ Hooks.on("preCreateChatMessage", (msg, options, userId) => {
     msg.data.update(updates);
   }
 });
+
+Hooks.on("init", () => {
+  // Register settings to hide Death Save from player's character sheet
+  game.settings.register("blind-death-saves", "hiddenDeathSaveStatus", {
+    name: "Hide Death Save Status",
+    hint: "Death Save status counters will be hidden for Players in their character sheets.",
+    type: Boolean,
+    default: false,
+    scope: "world",
+    config: true,
+    restricted: true,
+  });
+});
+
+// Remove death save counters from character sheet (only for Players)
+Hooks.on("renderActorSheet", async function (app, html, data) {
+  if (
+    game.settings.get("blind-death-saves", "hiddenDeathSaveStatus") &&
+    !game.user.isGM
+  ) {
+    let deathSaveCounters = $(html).find(
+      "div.counter.flexrow.death-saves > div.counter-value"
+    );
+    deathSaveCounters.remove();
+  }
+});
